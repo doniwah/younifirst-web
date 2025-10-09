a3d62bd29ee48392e0a4e0b7c79df9a5b4509a7a
doniwah <whyddoni@gmail.com>
2025-10-09 21:11:04 +0700

    update controller

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -6,10 +6,6 @@ class HomeController
 {
     public function index()
     {
-        $title = "Halaman Utama";
-        $message = "Selamat datang di Younifirst Web buatan Ridho!";
-
-        ob_start();
         include '../app/view/index.php';
         return ob_get_clean();
     }
