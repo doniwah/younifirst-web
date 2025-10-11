40d41797320c0c91f0a74ecf908d29d27238568a
doniwah <whyddoni@gmail.com>
2025-10-11 21:07:51 +0700

    add btn_nav

--- app/view/layouts/navbar.php
+++ app/view/layouts/navbar.php
@@ -2,6 +2,13 @@
         <div class="logo">
             <span>YouNiFirst</span>
         </div>
+        <div class="btn_nav">
+            <a href=""><i class="bi bi-house"></i>Dashboard</a>
+            <a href=""><i class="bi bi-trophy"></i>Kompetisi</a>
+            <a href=""><i class="bi bi-box-seam"></i>Lost & Found</a>
+            <a href=""><i class="bi bi-calendar4"></i>Event</a>
+            <a href=""><i class="bi bi-chat-left"></i>Forum</a>
+        </div>
         <a href="/login">
             <button class="login-btn" onclick="window.location.href='/login'">Login</button></a>
     </nav>
